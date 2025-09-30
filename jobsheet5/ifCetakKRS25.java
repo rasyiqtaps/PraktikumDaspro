e5101df3f1b009f988d519f45868f5bcdccf0c7f
mashhisyam <beliakunindrayanggg@gmail.com>
2025-09-30 14:26:07 +0700

    Modifikasi percobaan  2

--- jobsheet5/ifCetakKRS25.java
+++ jobsheet5/ifCetakKRS25.java
@@ -17,10 +17,8 @@ public class ifCetakKRS25 {
             
         } else {
 
+            System.out.println("registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
 
         }
-          
-        
-
     }
 }
