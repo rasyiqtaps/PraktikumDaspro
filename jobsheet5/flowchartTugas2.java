298259eb79f52b64d1ad49f366f6a8c9ca8845f4
mashhisyam <beliakunindrayanggg@gmail.com>
2025-09-30 12:13:47 +0700

    Tugas 2

---
+++ jobsheet5/flowchartTugas2.java
@@ -0,0 +1,18 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class flowchartTugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukan angka apaweh bebas: ");
+        int angka = sc. nextInt();
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+            
+        } else {
+            System.out.println("Ganjil");
+        }
+    }
+}
