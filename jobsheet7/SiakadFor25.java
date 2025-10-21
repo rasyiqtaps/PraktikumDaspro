665593ed102d3d43da82846930e1f42754ec4b3a
mashhisyam <beliakunindrayanggg@gmail.com>
2025-10-21 12:01:06 +0700

    Percobaan 1

--- jobsheet7/SiakadFor25.java
+++ jobsheet7/SiakadFor25.java
@@ -6,7 +6,7 @@ public class SiakadFor25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
-        int lulus = 0, tdkLulus = 0;
+
 
         for (int i = 1; i<= 10; i++) {
             System.out.print("masukan nilai mahasiswa ke-" + i + ": ");
@@ -17,16 +17,8 @@ public class SiakadFor25 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
-
-            if (nilai >=60) {
-                lulus++;
-            } else {
-                tdkLulus++;
-            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
-        System.out.println("Jumlah siswa Lulus: " + lulus);
-        System.out.println("jumlah siswa Tidak Lulus: " + tdkLulus);
     }
 }
