d2038e92aaa2c3f5ef537f182918f56af7eb767c
mashhisyam <beliakunindrayanggg@gmail.com>
2025-11-02 01:35:21 +0700

    Percobaan 2

---
+++ jobsheet8/square25.java
@@ -0,0 +1,19 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class square25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukan Nilai N = ");
+        int N = sc.nextInt();
+
+        for (int iOuter1 = 1; iOuter1 <= N; iOuter1++) {
+            for (int i = 1; i <= N; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
