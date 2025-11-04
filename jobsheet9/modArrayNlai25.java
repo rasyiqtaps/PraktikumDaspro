cda8b1e67ff2e978758616fd36820ce78350c185
mashhisyam <beliakunindrayanggg@gmail.com>
2025-11-04 10:23:00 +0700

    modifikasi percobaan 2

---
+++ jobsheet9/modArrayNlai25.java
@@ -0,0 +1,24 @@
+package jobsheet9;
+
+import java.util.Scanner;
+
+public class modArrayNlai25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukan nilai akhir ke-" + i + ": ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " Lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " Tidak Lulus!");
+            }
+        }
+    }   
+}
