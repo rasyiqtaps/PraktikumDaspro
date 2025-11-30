631960e7abd23cecf2917ad1f865ef654dedca59
mashhisyam <beliakunindrayanggg@gmail.com>
2025-11-30 20:36:08 +0700

    Percobaan 3

---
+++ jobsheet11/otherAlterKafe25.java
@@ -0,0 +1,44 @@
+package jobsheet11;
+
+import java.util.Scanner;
+
+public class otherAlterKafe25 {
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem){
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
+
+    public static void menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
+        }
+
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. Kopi Hitam - Rp. 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3, Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("=============================");
+        System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+
+    public static void main(String[] args) {
+        menu("andi", true);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("\nmasukan nomor menu yang ingin anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukab jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp." + totalHarga);
+    }
+}
